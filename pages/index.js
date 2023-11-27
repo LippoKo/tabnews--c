4789ef539b1e2402e971b59a7892b715c0ad4642
Luiz Lima <lippoko@LippoKo.local>
2023-11-27 21:19:53 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -3,3 +3,25 @@ function Home() {
 }
 
 export default Home;
+
+function testRoute() {
+  const express = require("express");
+  const router = express.Router();
+  const connectDB = require("../database/db");
+
+  router.get("/test", async (req, res) => {
+    try {
+      await connectDB();
+      res
+        .status(200)
+        .json({ success: true, message: "Successfully connected!" });
+    } catch (error) {
+      console.error("Connection failed!", error.message);
+      res.status(500).json({ success: false, error: error.message });
+    }
+  });
+
+  module.exports = router;
+}
+
+testRoute();
