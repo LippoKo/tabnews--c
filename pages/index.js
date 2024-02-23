f6bc235bffe6625c56c9a4ccb456cfd767fd4857
LippoKo <luizlimadevpro@gmail.com>
2024-02-23 22:11:32 +0000

    update home-

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,5 @@
 function Home() {
-  return <h1>Maezinha eu te amo mil milhoes!!!</h1>;
+  return <h1>Test 1</h1>;
 }
 
 export default Home;
-
-function testRoute() {
-  const express = require("express");
-  const router = express.Router();
-  const connectDB = require("../database/db");
-
-  router.get("/test", async (req, res) => {
-    try {
-      await connectDB();
-      res
-        .status(200)
-        .json({ success: true, message: "Successfully connected!" });
-    } catch (error) {
-      console.error("Connection failed!", error.message);
-      res.status(500).json({ success: false, error: error.message });
-    }
-  });
-
-  module.exports = router;
-}
-
-testRoute();
